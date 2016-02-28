1472069b08f5fed6045f2fbd2001a05aa3680ded
ljonathanl <jonathan.gourdin@gmail.com>
2016-02-28 21:47:59 +0100

    refactoring + socket.io

--- public/js/kanban.js
+++ public/js/kanban.js
@@ -1,3 +1,79 @@
+var kanban = {
+  backlog: {
+    evolution: {
+      path: 'backlog.evolution',
+      contents: [],
+    }, 
+    bug: {
+      path: 'backlog.bug',
+      contents: [
+        {type: 'task', title: 'My super task', id: 'tsk1'},
+        {type: 'task', title: 'My super task 2', id: 'tsk2'},
+      ],
+    },
+    technical: {
+      path: 'backlog.technical',
+      contents: []
+    }
+  },
+  todo: {
+      path: 'todo',
+      contents: [],
+  },
+  development: {
+    back: {
+      ongoing: {
+        path: 'development.back.ongoing',
+        contents: [{type: 'task', title: 'My super task', id: 'tsk3'}],
+      },
+      test: {
+        path: 'development.back.test',
+        contents: []
+      }
+    },
+    front: {
+      ongoing: {
+        path: 'development.front.ongoing',
+        contents: [],
+      },
+      test: {
+        path: 'development.front.test',
+        contents: []
+      }
+    },
+    cms: {
+      ongoing: {
+        path: 'development.cms.ongoing',
+        contents: [],
+      },
+      test: {
+        path: 'development.cms.test',
+        contents: [{type: 'task', title: 'My super task', id: 'tsk5'}]
+      }
+    },
+    other: {
+      ongoing: {
+        path: 'development.other.ongoing',
+        contents: [],
+      },
+      test: {
+        path: 'development.other.ongoing',
+        contents: []
+      }
+    },
+    done: {
+      path: 'done',
+      contents: [{type: 'task', title: 'My super task', id: 'tsk4'}]
+    }
+  }
+};
+
+var socket = io.connect('http://localhost');
+socket.on('news', function (data) {
+  //console.log(data);
+  //socket.emit('my other event', { my: 'data' });
+});
+
 var dragTemp = {};
 
 function getContentIndexById(id, contents) {
@@ -43,13 +119,14 @@ Vue.component('column', {
   template: '#column-template',
   props: {
     title: String,
-    contents: Array,
+    model: Object,
   },
 })
 
 Vue.component('container', {
   template: '#container-template',
   props: {
+    path: String,
     contents: Array
   },
   methods: {
@@ -67,41 +144,17 @@ Vue.component('container', {
       lastContainer.$remove(draggedObject);
       this.contents.splice(index, 0, draggedObject);
       dragTemp = {};
+
     }
   },
 })
 
 new Vue({
   el: 'body',
-  data: {
-    backlog: {
-      evolution: [],
-      bug: [
-        {type: 'task', title: 'My super task', id: 'tsk1'},
-        {type: 'task', title: 'My super task 2', id: 'tsk2'},
-      ],
-      technical: []
-    },
-    todo: [],
-    development: {
-      back: {
-        ongoing: [{type: 'task', title: 'My super task', id: 'tsk3'}],
-        test: []
-      },
-      front: {
-        ongoing: [],
-        test: []
-      },
-      cms: {
-        ongoing: [],
-        test: [{type: 'task', title: 'My super task', id: 'tsk5'}]
-      },
-      other: {
-        ongoing: [],
-        test: []
-      },
-      done: [{type: 'task', title: 'My super task', id: 'tsk4'}]
-    },
-    qualification: []
-  }
+  data: kanban
 })
+
+
+
+
+
